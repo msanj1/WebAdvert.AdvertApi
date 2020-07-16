423b20d4a807647195b6499b4132d25caed3fbb4
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2020-07-17 07:51:32 +1000

    Partial implementation of the AdvertApi

---
+++ AdvertApi.Models/ConfirmAdvertModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertApi.Models
+{
+    public class ConfirmAdvertModel
+    {
+        public int Id { get; set; }
+        public AdvertStatus Status { get; set; }
+    }
+}
